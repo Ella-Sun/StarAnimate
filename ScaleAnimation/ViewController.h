d62d672440d26f5af7b4d4e81fcbb5bb35d96dcc
sunhong <sunhong@ucfgroup.com>
2016-08-16 09:59:12 +0800

    Initial Commit

---
+++ ScaleAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScaleAnimation
+//
+//  Created by sunhong on 16/8/16.
+//  Copyright © 2016年 sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScaleAnimationTests/ScaleAnimationTests.m
@@ -0,0 +1,39 @@
+//
+//  ScaleAnimationTests.m
+//  ScaleAnimationTests
+//
+//  Created by sunhong on 16/8/16.
+//  Copyright © 2016年 sunhong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScaleAnimationTests : XCTestCase
+
+@end
+
+@implementation ScaleAnimationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
